5499ca8d7ef374084850bd51008f906fdfbff21d
Zenteno <x.zenteno.a@gmail.com>
2019-05-02 00:27:42 -0400

    Agrega medoo, para conexion a base de datos.

--- src/routes.php
+++ src/routes.php
@@ -7,29 +7,19 @@ use Slim\Http\Response;
 use Slim\Views\Twig;
 use Slim\Http\Environment;
 use Slim\Views\TwigExtension;
+use Medoo\Medoo;
 
 return function (App $app) {
-    $container = $app->getContainer();
 
-	
-	// Registrar twig al contenedor de slimm
-	$container['view'] = function ($c) {
-		$view = new Twig('../templates');
-	
-		// Instancia y añade la extensión
-		$router = $c->get('router');
-		$uri = Uri::createFromEnvironment(new Environment($_SERVER));
-		$view->addExtension(new TwigExtension($router, $uri));
-
-		return $view;
-	};
 	//puedes pasar valores por un arreglo
 	$app->get('/', function ($request, $response) {
+		$arr = $this->database->select('chiquillos', ['id', 'nombre']);
+
 		$arreglo = ["hola","soy","un","arreglo"];
-		$arreglo1 = ["hola","soy","otro","arreglo"];
+	
 		return $this->view->render($response, 'index.phtml', [
 			'arreglo' => $arreglo,
-			'otro_arreglo' => $arreglo1
+			'otro_arreglo' => $arr
 		]);
 	});
 
